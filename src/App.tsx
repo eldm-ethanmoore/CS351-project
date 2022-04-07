e7c90b8722c59abdd4aeef87756a42ad11f8ef06
trevorkerney <trevorkerney@hotmail.com>
2022-04-07 00:37:17 -0500

    init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+import './css/App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <Router>
+        
+      </Router>
+    </div>
+  );
+}
+
+export default App;
