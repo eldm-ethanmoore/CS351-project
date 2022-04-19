286cd431442ed37f20eafa0890b100a312b89465
Step <stevegans42@gmail.com>
2022-04-19 12:43:59 -0500

    test

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,12 @@ import './css/App.css';
 function App() {
   return (
     <div className="app">
-      <h1 className="titleText">Client Facing Dashboard</h1>
+      <h1 className="titleText">User Facing Dashboard</h1>
     </div>
   );
 }
-
+/*
+  git config --global user.email "you@example.com"
+  git config --global user.name "Your Name"
+*/
 export default App;
